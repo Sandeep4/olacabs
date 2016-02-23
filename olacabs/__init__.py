1799b63cf64346eef74a5381b1524b27881b4bcc
Sandeep Kumar <4sandeep.kumar@gmail.com>
2016-02-23 14:04:35 +0530

    add client to init

--- olacabs/__init__.py
+++ olacabs/__init__.py
@@ -1,6 +1,8 @@
 __author__ = 'sandeep'
 __title__ = 'Olacabs Python Client'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 # Version synonym
 VERSION = __version__
+
+from .client import OlaCabsClient
